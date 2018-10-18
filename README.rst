cdcbecf25592ffe5c07a9bfba3dea198b7320de9
harshilonline <harshilonline@gmail.com>
2018-10-17 19:16:12 -0700

    [ChargingStation] Commit to add setup hook and Readme
        -- Added setup.py in order to create a distribution package
        -- README.rst that has instructions on how to install the package and run it

---
+++ README.rst
@@ -0,0 +1,16 @@
+Charging Station Map
+==============================================
+
+This package provides a program for mapping charging station.
+
+Install the package
+====================
+sudo pip install chargingstation-0.1.dev0.tar.gz
+
+Example Usage for the program
+=========================
+Run below command:
+
+python charging_station_map.py
+
+This should open a browser window with map of charging stations
---
+++ setup.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2018. All rights reserved.
+
+from codecs import open
+from os import path
+import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+requirements = ["python-dateutil>=2.1,<3.0.0",
+                "docutils>=0.10",
+                "plotly",
+                "requests>=2.9.1"]
+
+setup(
+    name='chargingstation',
+    version='0.1dev',
+    description='Charging Station Map',
+    long_description=long_description,
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: System Administrators',
+        'License :: OSI Approved :: Apache License 2.0',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7,'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ],
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=requirements
+)
